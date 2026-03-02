02bd4214693e14707869201975096cd6adcdbdf1
saeed saeed <saeed.mohammad.yasser.saleh@gmail.com>
2026-03-02 10:03:50 +0300

    Added the feature of reading all withdrawal and deposit transactions

---
+++ DigitalWallet.API/Controllers/TransactionsController.cs
@@ -0,0 +1,31 @@
+﻿using DigitalWallet.Application.CQRS.Queries.Transactions;
+using DigitalWallet.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace DigitalWallet.API.Controllers
+{
+    [Route("api/Transactions")]
+    [ApiController]
+    public class TransactionsController(IMediator mediator) : ControllerBase
+    {
+        /*READ ALL Transactions*/
+        /// <summary>
+        /// Retrieves a collection of all available Transactions.
+        /// </summary>
+        /// <returns>An <see cref="IActionResult"/> containing a list of Transactions if any exist; otherwise, a response indicating
+        /// that no Transactions were found.</returns>
+        /// <response code="200">Transactions were retrieved successfully.</response>
+        /// <response code="404">No Transactions were found.</response>
+        [HttpGet(Name = "ReadAllTransactions")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Transactions were retrieved successfully.", typeof(TransactionEntity))]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "No Transactions were found.")]
+        public async Task<IActionResult> ReadAllTransactions()
+        {
+            ReadAllTransactionsQuery query = new();
+            var result = await mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
---
+++ DigitalWallet.Application/CQRS/Queries/Transactions.cs
@@ -0,0 +1,20 @@
+﻿using DigitalWallet.Domain.Entities;
+using DigitalWallet.Domain.Interfaces.Repositories;
+using MediatR;
+
+namespace DigitalWallet.Application.CQRS.Queries.Transactions
+{
+    public record ReadAllTransactionsQuery() : IRequest<IEnumerable<TransactionEntity>>;
+    public class ReadAllTransactionsQueryHandler : IRequestHandler<ReadAllTransactionsQuery, IEnumerable<TransactionEntity>>
+    {
+        private readonly ITransactionRepository _transactionRepository;
+        public ReadAllTransactionsQueryHandler(ITransactionRepository transactionRepository)
+        {
+            _transactionRepository = transactionRepository;
+        }
+        public async Task<IEnumerable<TransactionEntity>> Handle(ReadAllTransactionsQuery request, CancellationToken cancellationToken)
+        {
+            return await _transactionRepository.ReadAllTransactionsAsync();
+        }
+    }
+}
